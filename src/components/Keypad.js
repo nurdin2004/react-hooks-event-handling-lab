e9bc9656e1c2c4f6d5457bfb12b678f47a1e8303
nurdin2004 <adiaderkisohouf@gmail.com>
2025-04-24 11:06:10 +0300

    Update Keypad.js

--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,9 +1,16 @@
-// Code Keypad Component Here
-
-function Keypad (){
+function Keypad() {
+    function handleChange() {
+      console.log('Entering password...');
+    }
+  
     return (
-        <div></div>
-    )
-}
-
-export default Keypad;
+      <div>
+        <input 
+          type="password" 
+          onChange={handleChange}
+        />
+      </div>
+    );
+  }
+  
+  export default Keypad;
